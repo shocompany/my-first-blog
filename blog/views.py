75324715e4f41705a73712c3aabc5552ea068a1d
Your Name <you@example.com>
2020-04-01 02:35:48 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,21 @@ from django.shortcuts import render, get_object_or_404#与えられたpkのPost�
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post#同じフォルダにあるので.+modelsだけで表示が済む(.pyは必要ない)
+from .forms import PostForm
 
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
