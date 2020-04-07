386285bd8204b0eb6a9b0e317eb52144feacf2e9
Your Name <you@example.com>
2020-04-07 14:34:03 +0900

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o59@oxe^!xlc0hc3(#ee7#@(*2g(8(r22df86(d@5muq^ahgyj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False#Trueだった
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
