0e43136333b737da1502180f312c6c471693cd49
Your Name <you@example.com>
2020-04-08 16:20:43 +0900

    Changed the HTML for the site.

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
         return self.title
 
     def approved_comments(self):
-    return self.comments.filter(approved_comment=True)
+        return self.comments.filter(approved_comment=True)
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
--- blog/views.py
+++ blog/views.py
@@ -75,7 +75,7 @@ def add_comment_to_post(request, pk):
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
-    @login_required
+@login_required
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
